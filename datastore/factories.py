e50b40ec1c103a4a3eb385960d377c6074372971
Amir Mehr <amir.mehr@showpass.com>
2024-02-25 17:01:22 -0700

    add: factories

---
+++ datastore/factories.py
@@ -0,0 +1,20 @@
+from core.base.factory import BaseModelFactory
+from core.settings import GDRIVE_SERVICE_ACCOUNT, GDRIVE_TEST_FOLDER_ID
+
+from datastore.models import Config, Folder
+
+
+class ConfigFactory(BaseModelFactory):
+    class Meta:
+        model = Config
+
+    credentials = GDRIVE_SERVICE_ACCOUNT
+    email = "test@test.com"
+
+
+class FolderFactory(BaseModelFactory):
+    class Meta:
+        model = Folder
+
+    folder_id = GDRIVE_TEST_FOLDER_ID
+    config = ConfigFactory()
