38a29f81d0d0d83232fa5f766e2b9a0636ff1ef7
Amir Mehr <amir.mehr@showpass.com>
2024-03-02 15:19:51 -0700

    add: make FolderLoader to FreshDocumentsMetaDataLoader and RelatedDocumentsObjectLoader

--- datastore/services/syncer/folder/loader.py
+++ datastore/services/syncer/folder/loader.py
@@ -1,17 +1,19 @@
 from datastore.services.syncer.base import BaseLoader
 
 
-class FolderLoader(BaseLoader):
+class FreshDocumentsMetaDataLoader(BaseLoader):
     def __init__(self, folder):
         self.folder = folder
         self.reader = self.folder.config.google_drive_reader()
 
-    def _load_docs_from_reader(self):
-        self.fresh_docs = self.reader.load_data(folder_id=self.folder.folder_id)
+    def load_data(self):
+        self.docs = self.reader.load_file_meta_data(folder_id=self.folder.folder_id)
 
-    def _load_related_docs(self):
-        self.related_docs = self.folder.documents.all()
+
+class RelatedDocumentsObjectLoader(BaseLoader):
+    def __init__(self, folder):
+        self.folder = folder
+        self.reader = self.folder.config.google_drive_reader()
 
     def load_data(self):
-        self._load_docs_from_reader()
-        self._load_related_docs()
+        self.docs = self.folder.documents.all()
