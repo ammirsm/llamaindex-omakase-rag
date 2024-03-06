8e336f9937565482516da33fa6e8fd56461a8eb2
Amir Mehr <amir.mehr@showpass.com>
2024-03-06 08:22:25 -0700

    add auths to rest framework.

--- core/settings.py
+++ core/settings.py
@@ -153,4 +153,12 @@ AUTH_USER_MODEL = "uac.UACUser"
 # CHUNK EMBEDDING SIZE
 EMBEDDING_SIZE = os.environ.get("EMBEDDING_SIZE", 384)
 
-REST_FRAMEWORK = {"DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination", "PAGE_SIZE": 10}
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
